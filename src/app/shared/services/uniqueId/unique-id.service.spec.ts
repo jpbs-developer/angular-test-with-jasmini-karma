92f91c292008bf10d73e9daf94efd73b1045b959
jpbs-developer <jpbs1@aluno.ifal.edu.br>
2022-11-18 00:47:28 -0300

    [Dev][Feature] Like-widget component com acessibilidade

--- src/app/shared/services/uniqueId/unique-id.service.spec.ts
+++ src/app/shared/services/uniqueId/unique-id.service.spec.ts
@@ -1,17 +1,20 @@
-import { faFirstAid } from '@fortawesome/free-solid-svg-icons';
 import { UniqueIdService } from './unique-id.service';
 
 describe(`${UniqueIdService.name}`, () => {
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-    const service = new UniqueIdService();
+  let service: UniqueIdService = null;
 
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
+    should generate id when called with prefix`, () => {
     const id = service.generateUniqueIdWithPrefix('app');
-
     expect(id.startsWith('app-')).toBeTrue();
   });
 
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
-    const service = new UniqueIdService();
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
+    should not generate duplicate IDs when called multiple times`, () => {
     const numberOfGenerateIds = 100;
     const ids = new Set();
 
@@ -22,11 +25,21 @@ describe(`${UniqueIdService.name}`, () => {
     expect(ids.size).toBe(numberOfGenerateIds);
   });
 
-  it(`#${UniqueIdService.prototype.getNumberOfGenerateUniqueIds.name} should return the number of generatedIds when called`, () => {
-    const service = new UniqueIdService();
+  it(`#${UniqueIdService.prototype.getNumberOfGenerateUniqueIds.name} 
+    should return the number of generatedIds when called`, () => {
     service.generateUniqueIdWithPrefix('app');
     service.generateUniqueIdWithPrefix('app');
 
     expect(service.getNumberOfGenerateUniqueIds()).toBe(2);
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
+    should return throw when called with empty`, () => {
+    const ERRO_VALUES = [null, undefined, '', '0', '1'];
+    ERRO_VALUES.forEach((errorValue) =>
+      expect(() => service.generateUniqueIdWithPrefix(errorValue))
+        .withContext(`Empty value: ${errorValue}`)
+        .toThrow()
+    );
   });
 });
